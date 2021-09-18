b25bff0c5b46e8ec0797a8923450f4d652153b06
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-18 05:08:18 -0700

    edit app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import Contact from "./components/contact/Contact";
 import SideBar from "./components/sidebar";
 import { MyBooking } from "./components/myBooking/MyBooking";
 
-import logOut from "./components/logout/logOut";
+
 export const userSign = createContext();
 
 export const userContext = createContext();
